1c20db328f538ff5a73e7c01d0ed57f15abfc054
PedroMendes414 <pedrooliverio4@gmail.com>
2025-08-28 23:12:38 -0300

    Adicionei a soma e o seu teste

---
+++ src/calculadora.js
@@ -0,0 +1,35 @@
+function somarDoisValores (valor1, valor2) {
+    const resultado = valor1 + valor2;
+    return resultado;
+
+}
+
+function calcularMediaDeDoisValores (valor1, valor2) {
+    // 1. Soma todos os valores
+    const resultadoSomaDeDoisValores = somarDoisValores(valor1, valor2);
+
+    // 2. Divide pela quantidade de numeros passados
+    const resultadoMediaDoisValores = resultadoSomaDeDoisValores/2;
+
+    //3. Retornar o resultado
+    return resultadoMediaDoisValores;
+
+}
+
+function realizarSomaDosCamposDaTela () {
+    const valor1 = 1
+    const valor2 = 3
+
+    if(valor1 == 0 || valor2 == 0) {
+        console.log('Digite um valor maior que zero')
+    }
+
+    const resultado = somarDoisValores(valor1, valor2)
+
+    console.log(resultado)
+}
+
+module.exports = {
+    somarDoisValores,
+    calcularMediaDeDoisValores
+}
---
+++ tests/calculadora.test.js
@@ -0,0 +1,7 @@
+const {somarDoisValores, calcularMediaDeDoisValores} = require('../src/calculadora')
+
+const resultadoDaSoma = somarDoisValores(5, 3);
+const resultadoDaMedia = calcularMediaDeDoisValores(5, 3)
+
+console.log(resultadoDaSoma)
+console.log(resultadoDaMedia)
